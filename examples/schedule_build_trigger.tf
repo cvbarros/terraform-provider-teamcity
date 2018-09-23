8b0b8efdf64e6b7fbe0cd77ad1365a4c0271a01c
cvbarros <carlosvitorbarros@gmail.com>
2018-09-23 14:06:23 +0200

    TriggerSchedule options

--- examples/schedule_build_trigger.tf
+++ examples/schedule_build_trigger.tf
@@ -35,23 +35,53 @@ resource "teamcity_buildconfiguration" "nocode_triggered_build" {
 }
 
 resource "teamcity_build_trigger_schedule" "buildrelease_schedule_trigger" {
-    build_config_id = "${teamcity_buildconfiguration.nocode_build_release.id}"
+  build_config_id = "${teamcity_buildconfiguration.nocode_build_release.id}"
 
-    #daily, weekly (cron yet not supported)
-    schedule = "daily"
+  #daily, weekly (cron yet not supported)
+  schedule = "daily"
 
-    #Use values from TeamCity UI without abbreviation or (UTC-+X), like: America/Sao Paulo. Defaults to "SERVER", which uses the SERVER's timezone
-    timezone = "America/Sao Paulo"
+  #Use values from TeamCity UI without abbreviation or (UTC-+X), like: America/Sao Paulo. Defaults to "SERVER", which uses the SERVER's timezone
+  timezone = "America/Sao Paulo"
 
-    #Hour of the day that trigger will fire [0..23]
-    hour = 12
+  #Hour of the day that trigger will fire [0..23]
+  hour = 12
 
-    #Minute that the trigger will fire [0..59]. Optional, defaults to 0
-    minute = 37
+  #Minute that the trigger will fire [0..59]. Optional, defaults to 0
+  minute = 37
 
-    #Weekday for weekly triggers. Use only if schedule = "weekly". Possible values are the weekday names in english, like: "Monday", "Tuesday"...
-    weekday = "Saturday"
+  #Weekday for weekly triggers. Use only if schedule = "weekly". Possible values are the weekday names in english, like: "Monday", "Tuesday"...
+  weekday = "Saturday"
 
-    #Optional, only trigger on changes that match the rules. If none used, will trigger for any change.
-    rules = ["+:*", "-:*.md"]
+  #Optional, only trigger on changes that match the rules. If none used, will trigger for any change.
+  rules = ["+:*", "-:*.md"]
+
+  #Defaults to true on TeamCity UI - Queued build can be replaced with an already started build or a more recent queued build
+  queue_optimization = false
+
+  #Triggers on all compatible agents - Default: false
+  on_all_compatible_agents = true
+
+  #Trigger only if watched build changes - Default: false
+  with_pending_changes_only = true
+
+  #Promote watched build if there is a dependency (snapshot or artifact) on its build configuration. Default: true
+  promote_watched_build = false
+
+  #Delete all files in checkout directory before the build - Default: false
+  enforce_clean_checkout = true
+  
+  #Delete all files in checkout directory before the build also for snapshot dependencies. Default: false
+  enforce_clean_checkout_dependencies = true
+
+  #Trigger only if a given build configuration has pending changes. Default: false. If set to true, watched_build_config_id must be set
+  only_if_watched_changes = true
+
+  #Configures the watched build for this trigger
+  watched_build_config_id = "${teamcity_build_config.watched.id}"
+  
+  #Specify which version of the watched build should be considered. "lastFinished", "lastPinned", "lastSuccessful", "buildTag"
+  revision = "lastFinished"
+
+  #Used with revision = "buildTag", to specify the tag/branch for the watched build to be considered
+  watched_branch = "unstable"
 }
--- teamcity/resource_build_trigger_schedule.go
+++ teamcity/resource_build_trigger_schedule.go
@@ -59,6 +59,56 @@ func resourceBuildTriggerSchedule() *schema.Resource {
 				Elem:     &schema.Schema{Type: schema.TypeString},
 				Optional: true,
 			},
+			"enforce_clean_checkout": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"enforce_clean_checkout_dependencies": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"queue_optimization": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"on_all_compatible_agents": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"with_pending_changes_only": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"promote_watched_build": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"only_if_watched_changes": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"watched_build_config_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"revision": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "lastFinished",
+				ValidateFunc: validation.StringInSlice([]string{
+					string(api.LatestFinishedBuild),
+					string(api.LatestSuccessfulBuild),
+					string(api.LastBuildFinishedWithTag),
+				}, false),
+			},
+			"watched_branch": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "<default>",
+			},
 		},
 	}
 }
@@ -83,7 +133,13 @@ func resourceBuildTriggerScheduleCreate(d *schema.ResourceData, meta interface{}
 	rules := expandStringSlice(d.Get("rules").([]interface{}))
 	schedule := d.Get("schedule").(string)
 	weekday, _ := parseWeekday(d.Get("weekday").(string))
-	dt, err := api.NewTriggerSchedule(schedule, buildConfigID, weekday, uint(hour), uint(minute), timezone, rules, api.NewTriggerScheduleOptions())
+
+	opt, err := expandTriggerScheduleOptions(d)
+	if err != nil {
+		return err
+	}
+
+	dt, err := api.NewTriggerSchedule(schedule, buildConfigID, weekday, uint(hour), uint(minute), timezone, rules, opt)
 
 	if err != nil {
 		return err
@@ -135,7 +191,12 @@ func resourceBuildTriggerScheduleRead(d *schema.ResourceData, meta interface{}) 
 			return err
 		}
 	}
-
+	flatOpt := flattenTriggerScheduleOptions(dt.Options)
+	for k, v := range flatOpt {
+		if err := d.Set(k, v); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -148,4 +209,68 @@ func resourceBuildTriggerScheduleDelete(d *schema.ResourceData, meta interface{}
 	ts := client.TriggerService(d.Get("build_config_id").(string))
 
 	return ts.Delete(d.Id())
+}
+
+func expandTriggerScheduleOptions(d *schema.ResourceData) (*api.TriggerScheduleOptions, error) {
+	opt := api.NewTriggerScheduleOptions()
+
+	if v, ok := d.GetOkExists("queue_optimization"); ok {
+		opt.QueueOptimization = v.(bool)
+	}
+	if v, ok := d.GetOkExists("on_all_compatible_agents"); ok {
+		opt.BuildOnAllCompatibleAgents = v.(bool)
+	}
+	if v, ok := d.GetOkExists("with_pending_changes_only"); ok {
+		opt.BuildWithPendingChangesOnly = v.(bool)
+	}
+	if v, ok := d.GetOkExists("promote_watched_build"); ok {
+		opt.PromoteWatchedBuild = v.(bool)
+	}
+	if v, ok := d.GetOkExists("enforce_clean_checkout"); ok {
+		opt.EnforceCleanCheckout = v.(bool)
+	}
+	if v, ok := d.GetOkExists("enforce_clean_checkout_dependencies"); ok {
+		opt.EnforceCleanCheckoutForDependencies = v.(bool)
+	}
+	if v, ok := d.GetOkExists("only_if_watched_changes"); ok {
+		opt.TriggerIfWatchedBuildChanges = v.(bool)
+	}
+	if v, ok := d.GetOkExists("watched_build_config_id"); ok {
+		opt.RevisionRuleSourceBuildID = v.(string)
+	}
+	if v, ok := d.GetOkExists("revision"); ok {
+		opt.RevisionRule = api.ArtifactDependencyRevision(v.(string))
+	}
+	if v, ok := d.GetOkExists("watched_branch"); ok {
+		opt.RevisionRuleBuildBranch = v.(string)
+	}
+
+	return opt, nil
+}
+
+func flattenTriggerScheduleOptions(dt *api.TriggerScheduleOptions) map[string]interface{} {
+	out := make(map[string]interface{})
+	out["queue_optimization"] = dt.QueueOptimization
+	out["promote_watched_build"] = dt.PromoteWatchedBuild
+	out["with_pending_changes_only"] = dt.BuildWithPendingChangesOnly
+	out["revision"] = dt.RevisionRule
+	out["watched_branch"] = dt.RevisionRuleBuildBranch
+
+	if dt.BuildOnAllCompatibleAgents {
+		out["on_all_compatible_agents"] = dt.BuildOnAllCompatibleAgents
+	}
+	if dt.EnforceCleanCheckout {
+		out["enforce_clean_checkout"] = dt.EnforceCleanCheckout
+	}
+	if dt.EnforceCleanCheckoutForDependencies {
+		out["enforce_clean_checkout_dependencies"] = dt.EnforceCleanCheckoutForDependencies
+	}
+	if dt.TriggerIfWatchedBuildChanges {
+		out["only_if_watched_changes"] = dt.TriggerIfWatchedBuildChanges
+	}
+	if dt.RevisionRuleSourceBuildID != "" {
+		out["watched_build_config_id"] = dt.RevisionRuleSourceBuildID
+	}
+
+	return out
 }
