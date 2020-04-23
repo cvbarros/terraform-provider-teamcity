9ef3af5438cad5f62fa4885c22648524406f2839
tombuildsstuff <git@ibuildstuff.co.uk>
2020-04-23 16:31:53 +0200

    New Resource: `teamcity_agent_pool_project_assignment`
    This allows managing an assignment between an Agent Pool and a Project

--- teamcity/provider.go
+++ teamcity/provider.go
@@ -24,6 +24,7 @@ func Provider() terraform.ResourceProvider {
 			"teamcity_feature_commit_status_publisher": resourceFeatureCommitStatusPublisher(),
 			"teamcity_group":                           resourceGroup(),
 			"teamcity_group_role_assignment":           resourceGroupRoleAssignment(),
+			"teamcity_agent_pool_project_assignment":   resourceAgentPoolProjectAssignment(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"teamcity_agent_pool": dataSourceAgentPool(),
